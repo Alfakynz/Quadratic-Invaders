61c648e2fa8d7d6190d8e0f6d0e4e3fb3847c729
SonicX888 <kyllian.lim@gmail.com>
2025-10-09 21:17:36 +0200

    Creation of the window
    - Creation of the class Game
    - Creation of the function main

---
+++ src/game.py
@@ -0,0 +1,17 @@
+import pyxel
+
+class Game:
+    def __init__(self):
+        
+        self.window_width: int = 1000
+        self.window_height: int = 750
+
+        pyxel.init(self.window_width, self.window_height, title="Window-Kill")
+
+        pyxel.run(self.update, self.draw)
+    
+    def update(self):
+        pass
+
+    def draw(self):
+        pyxel.cls(0)
---
+++ src/main.py
@@ -0,0 +1,8 @@
+import pyxel
+from game import Game
+
+def main():
+    Game()
+
+if __name__ == "__main__":
+    main()
