d363a0932caae10e136646c413298ffafd1f3e33
Alfakynz <alfakynz.nyxaeris@gmail.com>
2025-10-10 12:56:30 +0200

    Add Character class (only interface)

---
+++ src/characters.py
@@ -0,0 +1,9 @@
+class Character:
+    def __init__(color: list[int], shape: str, hp: int, damage: int, speed: int, shield: int, speed_shoot: int):
+        self.color = color
+        self.shape = shape
+        self.hp = hp
+        self.damage = damage
+        self.speed = speed
+        self.shield = shield
+        self.speed_shoot = speed_shoot
