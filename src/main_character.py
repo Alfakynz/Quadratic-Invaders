95f026fe9e57b7e9c6e31db473afa9891405430e
SonicX888 <kyllian.lim@gmail.com>
2025-10-20 14:36:12 +0200

    Begining of main_character functions

--- src/main_character.py
+++ src/main_character.py
@@ -1,3 +1,4 @@
+import pyxel
 from characters import Character
 
 class MainCharacter(Character):
@@ -12,8 +13,29 @@ class MainCharacter(Character):
                 xp: int = 0) -> None:
         super().__init__(color, shape, hp, attack, speed, shield, speed_shoot, xp)
 
-    def shoot(self) -> None:
+        self.mc_x = 500
+        self.mc_y = 375
+
+    def move(self):
+
+        if pyxel.btn(pyxel.KEY_RIGHT) and self.mc_x<1000:
+            self.mc_x += 1
+        if pyxel.btn(pyxel.KEY_LEFT) and self.mc_x>0:
+            self.mc_x += -1
+        if pyxel.btn(pyxel.KEY_DOWN) and self.mc_y<750:
+            self.mc_y += 1
+        if pyxel.btn(pyxel.KEY_UP) and self.mc_y>0:
+            self.mc_y += -1
+
+    def shoot(self):
         pass
 
     def add_xp(self, amount: int) -> None:
         self.xp += amount
+
+    def update(self):
+        self.move()
+
+    def draw(self):
+        #pyxel.tri(self.mc_x, self.mc_y, x2, y2, x3, y3, col)
+        pass
